edaec2ac9ccb0ad05fbe466a09f118c69b707113
Davit <daavit@yahoo.com>
2021-12-07 16:27:15 -0500

    finished

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -8,7 +8,9 @@ const Comments = props => {
 
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((comment, id) => (
+        <Comment comment={comment} key={id} />
+      ))}
     </div>
   );
 };
